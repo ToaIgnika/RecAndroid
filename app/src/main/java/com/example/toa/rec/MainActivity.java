b66b7835dafac7df4433d0200c8f347cab00adaf
Toaignika <eugene.my88@gmail.com>
2018-04-26 21:41:24 -0700

    Created tabbed fragment activities.

--- app/src/main/java/com/example/toa/rec/MainActivity.java
+++ app/src/main/java/com/example/toa/rec/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.toa.rec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tab_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout =
+                (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Browse"));
+        tabLayout.addTab(tabLayout.newTab().setText("My Schedule"));
+        tabLayout.addTab(tabLayout.newTab().setText("Survey"));
+
+        final ViewPager viewPager =
+                (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new TabPagerAdapter
+                (getSupportFragmentManager(),
+                        tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/toa/rec/TabPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.toa.rec;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                BrowseFragment tab1 = new BrowseFragment();
+                return tab1;
+            case 1:
+                UserListFragment tab2 = new UserListFragment();
+                return tab2;
+            case 2:
+                SurveyFragment tab3 = new SurveyFragment();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
