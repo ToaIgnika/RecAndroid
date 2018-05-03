c6582a908f95b0751404784ec96c7d40fe389f29
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-05-03 10:21:14 -0700

    Added a button
    btn

--- app/src/main/java/com/example/toa/rec/Fragments/SurveyFragment.java
+++ app/src/main/java/com/example/toa/rec/Fragments/SurveyFragment.java
@@ -1,6 +1,8 @@
 package com.example.toa.rec.Fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -53,6 +56,32 @@ public class SurveyFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
+
+        Button submitButton = view.findViewById(R.id.SubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //Intent myIntent = new Intent(view.getContext(), agones.class);
+                //startActivityForResult(myIntent, 0);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Are you sure you'd like to submit this review?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+            }
+
+        });
 
 
         return view;
