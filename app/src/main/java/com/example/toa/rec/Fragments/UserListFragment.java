81a57df95630b3830848ded8b51c5788baf8a945
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-05-11 01:42:47 -0700

    Last Commit Before Friday
    Ok so what has changed is the API calls, the event class (not only has this class changed, Ryan is going to change it again before tomorrow) and the userlist which im working on populating
    
    Basically everything will have to change again becuase the table was changed again.
    
    What you should do tomorrow is try to get the events displaying on the calendar. There are a few exampels of using the API to pull data. One is in the logindialog. The other is in the userlist. The final one is in browse. They all follow the same structure. You must write the request in the dbOperation in the API and then define the case in the API file (both in php). Then you have to do a few things in java. First perform the network request (just copy this code from the previous classes) and then inside there on PostExecute call a method that redraws or draws the calendar / whatever you are trying to create.

--- app/src/main/java/com/example/toa/rec/Fragments/UserListFragment.java
+++ app/src/main/java/com/example/toa/rec/Fragments/UserListFragment.java
@@ -1,6 +1,9 @@
 package com.example.toa.rec.Fragments;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -9,13 +12,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.toa.rec.Api;
 import com.example.toa.rec.Event;
 import com.example.toa.rec.EventAdapter;
+import com.example.toa.rec.LoginHandler;
 import com.example.toa.rec.R;
+import com.example.toa.rec.RequestHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import static com.example.toa.rec.Api.CODE_GET_REQUEST;
+import static com.example.toa.rec.Api.CODE_POST_REQUEST;
 
 
 /**
@@ -46,13 +61,35 @@ public class UserListFragment extends Fragment {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-        prepareEventData();
+
+
+        HashMap<String, String> params = new HashMap<>();
+        SharedPreferences sharedPref = getContext().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String email = sharedPref.getString("email", "");
+        params.put("email", email);
+        PerformNetworkRequest pn = new PerformNetworkRequest(Api.URL_GET_USER_EVENTS, params, CODE_POST_REQUEST,getContext());
+        pn.execute();
+
 
 
         return view;
     }
 
-    private void prepareEventData() {
+    private void prepareEventData(JSONObject object) throws JSONException {
+
+        JSONArray arr = object.getJSONArray("events");
+        //iterate thru them and add
+
+        System.out.println("The object is" + object);
+
+        for(int i = 0; i < arr.length(); i++) {
+            System.out.println("There is an event" + arr.get(i));
+            Event event = new Event();
+            JSONObject eventObject = (JSONObject) arr.get(i);
+            event.fromJson(eventObject);
+            eventList.add(event);
+        }
+
 
         /*
         Event movie = new Event(100,"Mad Max: Fury Road", "Action & Adventure");
@@ -71,4 +108,65 @@ public class UserListFragment extends Fragment {
         mAdapter.notifyDataSetChanged();
     }
 
+
+    /*ALEX: Performs a request using the php scripts to the databsae*/
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+        Context c;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode, Context c) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+            this.c = c;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //   progressBar.setVisibility(View.VISIBLE);
+            System.out.println("We are in pre-execute");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            // progressBar.setVisibility(View.GONE);
+
+            System.out.println("in post execute" + s);
+            try {
+                //JSONObject object =
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    // Toast.makeText(c, object.getString("message"), Toast.LENGTH_SHORT).show();
+                    prepareEventData(object);
+                } else {
+
+                }
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(c, "SHIT FUCKED UP", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler requestHandler = new RequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
+    }
 }
