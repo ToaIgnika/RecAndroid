914c3fb622726765d13d2d8b69560d197d7f3867
Toaignika <eugene.my88@gmail.com>
2018-05-16 01:53:33 -0700

    Minor changes

--- app/src/main/java/com/example/toa/rec/Dialogs/EventDetailsDialog.java
+++ app/src/main/java/com/example/toa/rec/Dialogs/EventDetailsDialog.java
@@ -2,14 +2,24 @@ package com.example.toa.rec.Dialogs;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.toa.rec.Event;
 import com.example.toa.rec.R;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EventDetailsDialog extends Dialog implements View.OnClickListener{
     public Activity c;
@@ -57,19 +67,62 @@ public class EventDetailsDialog extends Dialog implements View.OnClickListener{
     }
 
     private void setEvents() {
+        SimpleDateFormat df = new SimpleDateFormat("MMM dd");
+        long lstamp =Long.parseLong(e.getEventDay() + "000") ;
+        Date eTime =new Date(lstamp);
+
         TextView holder;
+        ImageView img_holder;
         holder = (TextView) findViewById(R.id.tv_instructor_name);
-        holder.setText(e.getFirstname());
+        holder.setText(e.getFirstname() + " " + e.getLastname());
         holder = (TextView) findViewById(R.id.tv_instructor_info);
         holder.setText(e.getBio());
+
 
         holder = (TextView) findViewById(R.id.tv_class_name);
         holder.setText(e.getClassName());
         holder = (TextView) findViewById(R.id.tv_class_date);
-        holder.setText(e.getEventDay());
+        holder.setText(df.format(eTime)  + ", " + e.getBeginHour() + ":" + e.getBeginMin() + "-" + e.getEndHour() + ":" + e.getEndMin());
         holder = (TextView) findViewById(R.id.tv_class_location);
         holder.setText(e.getClassLocation());
         holder = (TextView) findViewById(R.id.tv_class_details);
         holder.setText(e.getClassDescription());
+
+
+        try {
+            new DownloadImageTask((ImageView) findViewById(R.id.iv_instructor)).execute("https://www.w3schools.com/bootstrap4/img_avatar5.png");
+            new DownloadImageTask((ImageView) findViewById(R.id.iv_class_cover)).execute("https://thumbs-prod.si-cdn.com/1aYkS8dARZZQSJMpPIY6sFlMUls=/800x600/filters:no_upscale():focal(423x276:424x277)/https://public-media.smithsonianmag.com/filer/c6/3b/c63ba994-68b6-4052-84bd-fb9eddce7cba/istock-499925702.jpg");
+            //new DownloadImageTask((ImageView) findViewById(R.id.iv_instructor)).execute(e.getPhotoURL());
+            //new DownloadImageTask((ImageView) findViewById(R.id.iv_class_cover)).execute(e.getClassImageURL());
+        } catch (Exception e) {
+            System.out.println("Img loader e: " + e);
+        }
+
+
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
+
